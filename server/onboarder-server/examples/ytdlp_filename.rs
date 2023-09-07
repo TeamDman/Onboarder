afbe53a67897083b45993a876ee673718d1de30f
TeamDman <TeamDman9201@gmail.com>
2023-09-07 17:27:47 -0400

    add examples showing getting filename predicitons from yt-dlp

---
+++ server/onboarder-server/examples/ytdlp_filename.rs
@@ -0,0 +1,28 @@
+
+fn get_ytdlp_filename(url: &str) -> Result<String, String> {
+    let output = std::process::Command::new("yt-dlp")
+        .arg("--print")
+        .arg("filename")
+        .arg("--cookies-from-browser")
+        .arg("edge")
+        .arg("--windows-filenames")
+        .arg("--embed-metadata")
+        .arg(url)
+        .output()
+        .expect("Failed to execute command");
+
+    if output.status.success() {
+        let fname = String::from_utf8_lossy(&output.stdout);
+        let fname_trimmed = fname.trim_end_matches('\n');
+        Ok(fname_trimmed.to_string())
+    } else {
+        Err(String::from_utf8_lossy(&output.stderr).to_string())
+    }
+}
+
+fn main() {
+    // Run the full command
+    let url = "https://www.youtube.com/watch?v=EdYwBk2qe7Q";
+    let fname = get_ytdlp_filename(url).unwrap();
+    println!("Filename: \"{}\"", fname);
+}
---
+++ server/onboarder-server/examples/ytdlp_filename_async.rs
@@ -0,0 +1,29 @@
+async fn get_ytdlp_filename(url: &str) -> Result<String, String> {
+    let output = tokio::process::Command::new("yt-dlp")
+        .arg("--print")
+        .arg("filename")
+        .arg("--cookies-from-browser")
+        .arg("edge")
+        .arg("--windows-filenames")
+        .arg("--embed-metadata")
+        .arg(url)
+        .output()
+        .await
+        .expect("Failed to execute command");
+
+    if output.status.success() {
+        let fname = String::from_utf8_lossy(&output.stdout);
+        let fname_trimmed = fname.trim_end_matches('\n');
+        Ok(fname_trimmed.to_string())
+    } else {
+        Err(String::from_utf8_lossy(&output.stderr).to_string())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Run the full command
+    let url = "https://www.youtube.com/watch?v=EdYwBk2qe7Q";
+    let fname = get_ytdlp_filename(url).await.unwrap();
+    println!("Filename: \"{}\"", fname);
+}
