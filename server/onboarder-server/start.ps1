cde13697a213eb534d76df2e36397ef06a70de67
TeamDman <TeamDman9201@gmail.com>
2024-06-05 00:36:33 -0400

    add pause

--- server/onboarder-server/start.ps1
+++ server/onboarder-server/start.ps1
@@ -1,6 +1,8 @@
 cargo run -- `
---port 5876 `
+--port 21690 `
 --notes-dir "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Notes" `
 --downloads-dir "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Downloads" `
 --search-dirs "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Downloads","C:\Users\TeamD\OneDrive\Videos\"
+Pause
+echo "bruh"
 Pause
