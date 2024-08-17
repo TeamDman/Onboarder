2e4c449a0ce1e93a3d8e3ba2b53750f1085309fe
TeamDman <TeamDman9201@gmail.com>
2024-08-17 02:02:17 -0400

    change port

--- server/onboarder-server/start.ps1
+++ server/onboarder-server/start.ps1
@@ -1,8 +1,8 @@
 cargo run -- `
---port 21690 `
+--port 25569 `
 --notes-dir "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Notes" `
 --downloads-dir "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Downloads" `
 --search-dirs "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Downloads","C:\Users\TeamD\OneDrive\Videos\"
 Pause
 echo "bruh"
-Pause
+exit 1
