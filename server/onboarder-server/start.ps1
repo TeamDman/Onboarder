a8e88c84bd098ed78c424f76f0c5f42a8d1153ff
TeamDman <TeamDman9201@gmail.com>
2025-05-16 21:51:30 -0400

    avoid rebuilding if already have binary

--- server/onboarder-server/start.ps1
+++ server/onboarder-server/start.ps1
@@ -1,13 +1,32 @@
-# .\target\debug\onboarder-server.exe `
+$releaseExe = "target/release/onboarder-server.exe"
+$debugExe = "target/debug/onboarder-server.exe"
+
 $port = 25569
 if (Test-Path "port.txt") {
     $port = Get-Content "port.txt"
 }
-cargo run -- `
---port $port `
---notes-dir "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Notes" `
---downloads-dir "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Downloads" `
---search-dirs "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Downloads","C:\Users\TeamD\OneDrive\Videos\"
+
+$notesDir = "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Notes"
+$downloadsDir = "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Downloads"
+$searchDirs = "C:\Users\TeamD\OneDrive\Documents\Ideas\YouTube\Downloads,C:\Users\TeamD\OneDrive\Videos\"
+
+$exeArgs = @(
+    "--port", $port,
+    "--notes-dir", $notesDir,
+    "--downloads-dir", $downloadsDir,
+    "--search-dirs", $searchDirs
+)
+
+if (Test-Path $releaseExe) {
+    & $releaseExe @exeArgs
+}
+elseif (Test-Path $debugExe) {
+    & $debugExe @exeArgs
+}
+else {
+    cargo run -- @exeArgs
+}
+
 Pause
-echo "bruh"
+Write-Output "bruh"
 exit 1
